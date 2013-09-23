3458eb7cbf5f41d58084d17a2b1b28ebdb3eb23a
Shaun Appleton <sappleto@localhost.localdomain>
2013-09-23 12:44:02 +0100

    remove unneeded double quotes

--- mr_word_count/runNodes.bat
+++ mr_word_count/runNodes.bat
@@ -1,7 +1,7 @@
 rem starts 4 nodes in seperate cmd windows
 
 
-start "node0" "java -Djgroups.bind_addr=127.0.0.1 -Djava.net.preferIPv4Stack=true -cp target/classes/;target/dependency/* org.infinispan.quickstart.mrwordcount.distribution.Node0"
-start "node1" "java -Djgroups.bind_addr=127.0.0.1 -Djava.net.preferIPv4Stack=true -cp target/classes/;target/dependency/* org.infinispan.quickstart.mrwordcount.distribution.Node1"
-start "node2" "java -Djgroups.bind_addr=127.0.0.1 -Djava.net.preferIPv4Stack=true -cp target/classes/;target/dependency/* org.infinispan.quickstart.mrwordcount.distribution.Node2"
-start "node3" "java -Djgroups.bind_addr=127.0.0.1 -Djava.net.preferIPv4Stack=true -cp target/classes/;target/dependency/* org.infinispan.quickstart.mrwordcount.distribution.Node3"
+start "node0" java -Djgroups.bind_addr=127.0.0.1 -Djava.net.preferIPv4Stack=true -cp target/classes/;target/dependency/* org.infinispan.quickstart.mrwordcount.distribution.Node0
+start "node1" java -Djgroups.bind_addr=127.0.0.1 -Djava.net.preferIPv4Stack=true -cp target/classes/;target/dependency/* org.infinispan.quickstart.mrwordcount.distribution.Node1
+start "node2" java -Djgroups.bind_addr=127.0.0.1 -Djava.net.preferIPv4Stack=true -cp target/classes/;target/dependency/* org.infinispan.quickstart.mrwordcount.distribution.Node2
+start "node3" java -Djgroups.bind_addr=127.0.0.1 -Djava.net.preferIPv4Stack=true -cp target/classes/;target/dependency/* org.infinispan.quickstart.mrwordcount.distribution.Node3
